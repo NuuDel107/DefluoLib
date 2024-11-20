86b0b5fa5f52951a957633583cb590dee8e35f80
NuuDel107 <nuudeli.productions@gmail.com>
2024-11-20 09:56:16 +0200

    Modified FMODEventPlayback behavior
    Added ability to play events attached to a node and changed overall recommended way to use the node.

--- src/Resources/FMODEventPlayback.cs
+++ src/Resources/FMODEventPlayback.cs
@@ -4,6 +4,19 @@ using System.Linq;
 using Godot;
 using Godot.Collections;
 
+/// <summary>
+/// A resource containing arguments for playing an event.
+/// Can be used for various purposes.<br/><br/>
+/// For example, exporting a function with this resource as an argument
+/// allows for events to be played straight from an animation track:
+/// <code>
+/// public partial class Character : CharacterBody3D
+/// {
+///     // Calling this function from a call method track will play the event
+///     public void PlaySound(FMODEventPlayback eventPlayback) => eventPlayback.Play(this);
+/// }
+/// </code>
+/// </summary>
 [Tool]
 [GlobalClass]
 public partial class FMODEventPlayback : Resource
@@ -12,21 +25,27 @@ public partial class FMODEventPlayback : Resource
     public FMODEvent Event { get; set; }
 
     [Export]
+    public bool PlayAttached;
+
+    [Export]
     public Dictionary<string, Variant> Arguments = new();
 
-    public void Play()
+    public void Play(Node3D? nodeToAttach = null)
     {
         if (Event == null)
             return;
 
+        var args = Arguments.Select(x => (x.Key, x.Value)).ToArray();
         if (Engine.IsEditorHint())
         {
-            var args = Arguments.Select(x => (x.Key, x.Value)).ToArray();
             DefluoLib.FMODLister.PlayEvent(Event.Path, args);
         }
         else
         {
-            Event.Play(Arguments.Select(x => (x.Key, x.Value)).ToArray());
+            if (PlayAttached && nodeToAttach != null)
+                Event.PlayAttached(nodeToAttach, args);
+            else
+                Event.Play(args);
         }
     }
 }
